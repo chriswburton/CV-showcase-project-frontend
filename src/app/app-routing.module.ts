2d0356e4a922ed5f18436d70f48e2397e3e4cb16
Chris Burton <chrisburton@Chriss-MacBook-Pro.local>
2018-11-14 09:19:36 +0000

    Basic application structure + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {ApplicationsComponent} from './pages/applications/applications.component';
+import {LogOutComponent} from './pages/log-out/log-out.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'applications', component: ApplicationsComponent },
+  { path: 'log-in', component: LogOutComponent },
+  { path: 'log-out', component: LogOutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ApplicationsComponent } from './pages/applications/applications.component';
+import { LogOutComponent } from './pages/log-out/log-out.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ApplicationsComponent,
+    LogOutComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
